e41582c214af7cf9c254afdb6dd37fc92214d177
Jayshri3010 <jayshribkor30@gmail.com>
2022-10-16 21:06:46 +0530

    firstcommit

---
+++ Web-23-Variable/problem2.js
@@ -0,0 +1,6 @@
+let name = "Jayshri"
+console.log(name)
+let father = "Bhaskar"
+console.log(father)
+let mother = "Ranjana"
+console.log(mother)
---
+++ Web-23-Variable/problem3.js
@@ -0,0 +1,4 @@
+let name = "Jayshri";
+let age = 20;
+console.log(name,age);
+console.log(typeof(name), typeof(age));
