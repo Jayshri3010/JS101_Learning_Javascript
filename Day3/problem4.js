d28cc8a739244d8ecb3e6b6d6a27555a31d4006d
Jayshri3010 <jayshribkor30@gmail.com>
2022-10-19 08:10:37 +0530

    day3

---
+++ Day3/problem4.js
@@ -0,0 +1,11 @@
+let a = 18 ;
+let b = 45 ;
+let c = 27 ;
+
+if (a > b && a > c){
+  console.log("a is greatest")
+} else if (b > c && b > a){
+  console.log("b is greatest")
+} else {
+  console.log("c is greatest")
+}
