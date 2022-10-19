919b41f0449e288a9980b4055191005edac7387b
Jayshri3010 <jayshribkor30@gmail.com>
2022-10-19 07:53:22 +0530

    day2

---
+++ Day2/prblm3.js
@@ -0,0 +1,9 @@
+let a = 2000;
+let b = 3999;
+if (a>b){
+  console.log(a, "is greater");
+} else if (b>a){
+  console.log(b,"is greater")
+} else{
+  console.log("Both are equal")
+}
---
+++ Day2/problem1.js
@@ -0,0 +1,5 @@
+
+let numb = 15 ;
+if (numb % 3 == 0) {
+  console.log("multiple of 3")
+}
